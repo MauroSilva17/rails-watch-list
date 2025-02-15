843c5d940d15110d1a8b27ddddff11aa00374746
MauroSilva17 <m.henriquesilva18@gmail.com>
2025-02-15 13:07:50 +0100

    generate controllers

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,6 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  root "movies#index"
   resources :lists
   resources :movies
   # resources :list
